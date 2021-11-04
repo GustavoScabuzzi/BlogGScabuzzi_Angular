87905c216129fe977fdfb2a86641372a4742e95b
Gustavo Scabuzzi Gonçalves <34173963+GustavoScabuzzi@users.noreply.github.com>
2021-11-04 11:24:11 -0300

    [Feature Gustavo - Task: Criaçao component menu e rodape]

---
+++ src/app/model/Tema.ts
@@ -0,0 +1,5 @@
+export class Tema{
+    public idTema: number;
+    public tema: string;
+    public postagens: Postagem{}
+}
---
+++ src/app/model/Usuario.ts
@@ -0,0 +1,9 @@
+export class Usuario{
+    public email: string;
+    public idUsuario: number;
+    public nome: string;
+    public senha: string;
+    public foto: string;
+    public tipo: string;
+    public postagemUsuario: Postagem{}
+}
