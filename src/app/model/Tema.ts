d554a3d1dea82f773a033eeeb73c1380aa143ef1
Gustavo Scabuzzi Gonçalves <34173963+GustavoScabuzzi@users.noreply.github.com>
2021-11-12 01:32:17 -0300

    [Feature Gustavo - Task: Implementacao do get e post de temas]

--- src/app/model/Tema.ts
+++ src/app/model/Tema.ts
@@ -3,5 +3,5 @@ import { Postagem } from "./Postagem";
 export class Tema{
     public idTema: number;
     public tema: string;
-    public postagens: Postagem[]
+    public postagens: Postagem[];
 }
