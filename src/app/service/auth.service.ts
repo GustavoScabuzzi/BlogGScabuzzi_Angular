4edabb2dd655e92d7c85df4f0a4295aab913ab13
Gustavo Scabuzzi Gonçalves <34173963+GustavoScabuzzi@users.noreply.github.com>
2021-11-16 09:54:31 -0300

    [Feature Gustavo - Task: Metodos Post e components edit, delete, temaService e PostagemService adicionados]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -20,6 +20,10 @@ export class AuthService {
     return this.http.post<Usuario>('https://bloggscabuzzi.herokuapp.com/usuarios/cadastrar', usuario)
   }
 
+  getByIdUser(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>('https://bloggscabuzzi.herokuapp.com/usuarios/${id}')
+  }
+
   logado(){
     let ok: boolean = false
 
