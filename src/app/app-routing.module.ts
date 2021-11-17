223585736c6bde1e5a88152dfdc94ae0c2eaa19e
Gustavo Scabuzzi Gonçalves <34173963+GustavoScabuzzi@users.noreply.github.com>
2021-11-17 00:28:21 -0300

    [Feature Gustavo - Task: Implementaçao do put e delete de postagem]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { CadastrarComponent } from "./cadastrar/cadastrar.component";
+import { PostagemDeleteComponent } from "./delete/postagem-delete/postagem-delete.component";
 import { TemaDeleteComponent } from "./delete/tema-delete/tema-delete.component";
+import { PostagemEditComponent } from "./edit/postagem-edit/postagem-edit.component";
 import { TemaEditComponent } from "./edit/tema-edit/tema-edit.component";
 import { EntrarComponent } from "./entrar/entrar.component";
 import { InicioComponent } from "./inicio/inicio.component";
@@ -15,10 +17,15 @@ const routes: Routes = [
     {path: 'cadastrar', component: CadastrarComponent},
 
     {path: 'inicio', component: InicioComponent},
-    {path: 'tema', component: TemaComponent},
 
+    {path: 'tema', component: TemaComponent},
     {path: 'tema-edit/:id', component: TemaEditComponent},
-    {path: 'tema-delete/:id', component: TemaDeleteComponent}
+    {path: 'tema-delete/:id', component: TemaDeleteComponent},
+
+    {path: 'postagem-delete/:id', component: PostagemDeleteComponent},
+    {path: 'postagem-edit/:id', component: PostagemEditComponent}
+
+
 ];
 
 @NgModule({
--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -25,7 +25,19 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://bloggscabuzzi.herokuapp.com/postagens/todos', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://bloggscabuzzi.herokuapp.com/postagens/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://bloggscabuzzi.herokuapp.com/postagens/post', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://bloggscabuzzi.herokuapp.com/postagens/put', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`https://bloggscabuzzi.herokuapp.com/postagens/delete/${id}`, this.token)
   }
 }
